681238e6043f74b689d3ae557391699269d2e6f4
NiculescuAndrei <niculescuandrei8@yahoo.com>
2022-07-08 15:20:06 +0300

    Update DaciaEngineTest2.java

--- src/test/java/DaciaEngineTest2.java
+++ src/test/java/DaciaEngineTest2.java
@@ -7,6 +7,6 @@ public class DaciaEngineTest2 {
   public void testSpeedIncrement() {
     DaciaEngine engine = new DaciaEngine();
     engine.setAcceleration(100);
-    assertEquals(1.0, engine.getSpeedIncrement(), 0);
+    assertEquals(2.0, engine.getSpeedIncrement(), 0);
   }
 }
